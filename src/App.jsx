9f0cb5aba2eb36eea669739df6bfe37b4344bf01
DarenaSkip <skiparius@gmail.com>
2025-04-25 16:35:03 +0300

    Paruoštas pirmas prisijungimo vaizdas

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import MainPage from "./pages/MainPage";
 import LoginPage from "./pages/LoginPage";
+import MainPage from "./pages/MainPage";
 import RegisterPage from "./pages/RegisterPage";
 import ItemsPage from "./pages/ItemsPage";
 
@@ -8,8 +8,8 @@ export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/main" element={<MainPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/items" element={<ItemsPage />} />
       </Routes>
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,4 +1,54 @@
-export default function LoginPage() {
-    return <div>Login Page</div>;
-  }
+// export default function LoginPage() {
+//     return <div>Login Page</div>;
+//   }
   
+import { Box, Button, Container, TextField, Typography } from '@mui/material'
+
+export default function LoginPage() {
+  return (
+    <Container maxWidth="xs">
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Typography component="h1" variant="h5">
+          Prisijungimas
+        </Typography>
+        <Box component="form" noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="El. paštas"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Slaptažodis"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Prisijungti
+          </Button>
+        </Box>
+      </Box>
+    </Container>
+  )
+}
